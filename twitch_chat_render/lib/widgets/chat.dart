51d49df8dd302bb7f05a69a91d960061937b31e3
Nassim <nassim.abdelghani@imt-atlantique.net>
2021-12-23 07:38:19 +0100

    chat - play chat at normal speed

--- twitch_chat_render/lib/widgets/chat.dart
+++ twitch_chat_render/lib/widgets/chat.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:twitch_chat_render/models/chat_model.dart';
 import 'package:twitch_chat_render/services/amqp_interface.dart';
@@ -13,12 +15,29 @@ class Chat extends StatefulWidget {
 class _ChatState extends State<Chat> {
   final int _maxMessageCount = 200;
   late List<Comment>? comments;
+  int nextMessageIndex = 0;
+  Duration updatePeriod = const Duration(milliseconds: 300);
   bool init = false;
+  int? initTick;
+  ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     retrieveComments();
+    Timer.periodic(updatePeriod, (Timer timer) {
+      if (init) {
+        initTick ??= timer.tick;
+        setState(() {
+          double currentTime =
+              (timer.tick - initTick!) * updatePeriod.inMilliseconds / 1000;
+          while (nextMessageIndex < comments!.length - 1 &&
+              comments![nextMessageIndex].contentOffsetSeconds! < currentTime) {
+            nextMessageIndex++;
+          }
+        });
+      }
+    });
   }
 
   void retrieveComments() async {
@@ -28,13 +47,27 @@ class _ChatState extends State<Chat> {
     });
   }
 
+  List<Comment>? activeComments() {
+    return comments?.sublist(
+        (nextMessageIndex - _maxMessageCount) >= 0
+            ? (nextMessageIndex - _maxMessageCount)
+            : 0,
+        nextMessageIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      if (scrollController.hasClients) {
+        scrollController.jumpTo(scrollController.position.maxScrollExtent);
+      }
+    });
     return init
         ? ListView.builder(
-            itemCount: _maxMessageCount,
+            controller: scrollController,
+            itemCount: activeComments()?.length ?? 0,
             itemBuilder: (BuildContext context, int index) {
-              return ChatMessage(comment: comments?[0]);
+              return ChatMessage(comment: activeComments()?[index]);
             })
         : const Center(
             child: Text("Loading"),
