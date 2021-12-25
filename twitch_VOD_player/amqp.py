36c46e98f96a4c51e41645cd8e4131c987a68638
Nassim <nassim.abdelghani@imt-atlantique.net>
2021-12-25 22:26:59 +0100

    player - add types hint

--- twitch_VOD_player/amqp.py
+++ twitch_VOD_player/amqp.py
@@ -1,16 +1,25 @@
+from typing import Union
+
 import pika
+from pika.adapters.blocking_connection import BlockingChannel
 
 
-def init():
+def init() -> pika.BlockingConnection:
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     return connection
 
 
-def new_channel(connection):
+def new_channel(connection: pika.BlockingConnection) -> BlockingChannel:
     channel = connection.channel()
     channel.exchange_declare(exchange="topic_chat", exchange_type="topic")
     return channel
 
 
-def publish(channel, routing_key, body):
-    channel.basic_publish(exchange="topic_chat", routing_key=routing_key, body=body)
+def publish(
+    channel: BlockingChannel, routing_key: str, body: Union[str, bytes]
+) -> None:
+    channel.basic_publish(
+        exchange="topic_chat",
+        routing_key=routing_key,
+        body=body,  # type: ignore[arg-type]
+    )
--- twitch_VOD_player/topics.py
+++ twitch_VOD_player/topics.py
@@ -1,26 +1,30 @@
+from typing import Union
+
+from pika.adapters.blocking_connection import BlockingChannel
+
 from amqp import publish
 
 
-def json(channel, chat):
+def json(channel: BlockingChannel, chat: Union[str, bytes]) -> None:
     publish(channel, "json", chat)
     channel.close()
 
 
-def seek(channel, pos):
-    publish(channel, routing_key="sync.seek", body=str(pos))
+def seek(channel: BlockingChannel, pos: str) -> None:
+    publish(channel, routing_key="sync.seek", body=pos)
 
 
-def play(channel, pos):
-    publish(channel, routing_key="sync.play", body=str(pos))
+def play(channel: BlockingChannel, pos: str) -> None:
+    publish(channel, routing_key="sync.play", body=pos)
 
 
-def pause(channel, pos):
-    publish(channel, routing_key="sync.pause", body=str(pos))
+def pause(channel: BlockingChannel, pos: str) -> None:
+    publish(channel, routing_key="sync.pause", body=pos)
 
 
-def timer(channel, pos):
-    publish(channel, routing_key="sync.timer", body=str(pos))
+def timer(channel: BlockingChannel, pos: str) -> None:
+    publish(channel, routing_key="sync.timer", body=pos)
 
 
-def chatExit(channel):
+def chatExit(channel: BlockingChannel) -> None:
     publish(channel, routing_key="exit", body="")
