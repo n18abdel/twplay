89a51ed3fa597ac6d0263ce3be9da534283ba02c
Nassim <nassim.abdelghani@imt-atlantique.net>
2021-12-25 19:21:48 +0100

    player - reorganize imports

--- twitch_VOD_player/utils.py
+++ twitch_VOD_player/utils.py
@@ -4,8 +4,8 @@ import subprocess
 import tempfile
 import threading
 
+import topics
 from player import Player
-from topics import chatExit, json, timer
 
 
 def setup_exit_handler(callback):
@@ -30,13 +30,13 @@ def download_chat(vod_id):
 
 
 def send_chat_file(channel, chat):
-    json(channel, chat)
+    topics.json(channel, chat)
 
 
-def timer_loop(player: Player, channel, period):
+def setup_timer_loop(player: Player, channel, period):
     def timer_callback():
         if player.current_pos():
-            timer(channel, player.current_pos())
+            topics.timer(channel, player.current_pos())
 
     t = threading.Timer(period, timer_callback)
     t.start()
@@ -46,7 +46,7 @@ def timer_loop(player: Player, channel, period):
 def exit_callback(connection, channel, player: Player, timer):
     print("Exiting")
     timer.cancel()
-    chatExit(channel)
+    topics.chatExit(channel)
     print("Sent exit message to chat")
     connection.close()
     print("Closed AMQP connection")
