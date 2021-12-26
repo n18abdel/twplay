d5f6f107dd7349889e68e562368474d0d9a51f57
Nassim <nassim.abdelghani@imt-atlantique.net>
2021-12-26 18:27:55 +0100

    chat - add status in appbar

--- twitch_chat_render/lib/pages/home.dart
+++ twitch_chat_render/lib/pages/home.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:twitch_chat_render/services/app_status.dart';
 import 'package:twitch_chat_render/widgets/chat.dart';
+import 'package:twitch_chat_render/widgets/custom_app_bar.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Chat(),
+    return ChangeNotifierProvider(
+      create: (context) => AppStatus(),
+      child: const Scaffold(
+        appBar: CustomAppBar(),
+        body: Chat(),
+      ),
     );
   }
 }
