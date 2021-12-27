5e67615a6f5c27acaccfde00f24f188271487df7
Nassim <nassim.abdelghani@imt-atlantique.net>
2021-12-27 09:52:33 +0100

    chat - fix time calculation

--- twitch_chat_render/lib/widgets/chat.dart
+++ twitch_chat_render/lib/widgets/chat.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:twitch_chat_render/models/chat_model.dart';
 import 'package:twitch_chat_render/services/amqp_interface.dart';
-import 'package:twitch_chat_render/services/app_status.dart';
+import 'package:twitch_chat_render/models/app_status.dart';
 import 'package:twitch_chat_render/services/bttv_emotes.dart';
 import 'package:twitch_chat_render/services/twitch_badges.dart';
 import 'package:twitch_chat_render/services/twitch_cheer_emotes.dart';
@@ -19,25 +19,30 @@ class Chat extends StatefulWidget {
 
 class _ChatState extends State<Chat> {
   final int _maxMessageCount = 200;
-  Streamer? streamer;
-  List<Comment>? comments;
-  TwitchBadges? badges;
-  BTTVEmotes? bttvEmotes;
-  TwitchCheerEmotes? cheerEmotes;
+  Streamer? get streamer =>
+      Provider.of<AppStatus>(context, listen: false).streamer;
+  List<Comment>? get comments =>
+      Provider.of<AppStatus>(context, listen: false).comments;
+  TwitchBadges? get badges =>
+      Provider.of<AppStatus>(context, listen: false).badges;
+  BTTVEmotes? get bttvEmotes =>
+      Provider.of<AppStatus>(context, listen: false).bttvEmotes;
+  TwitchCheerEmotes? get cheerEmotes =>
+      Provider.of<AppStatus>(context, listen: false).cheerEmotes;
   int nextMessageIndex = 0;
   Duration updatePeriod = const Duration(milliseconds: 300);
   Timer? timer;
   double chatTime = 0;
   double chatSpeed = 1;
   double chatOffset = 0;
-  bool playing = false;
-  bool initStatus = false;
+  bool get playing => Provider.of<AppStatus>(context, listen: false).playing;
+  bool get initStatus =>
+      Provider.of<AppStatus>(context, listen: false).initStatus;
   ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    retrieveComments();
     setupSync();
     context.read<AppStatus>().addListener(() {
       var newOffset = context.read<AppStatus>().offset;
@@ -49,28 +54,24 @@ class _ChatState extends State<Chat> {
   }
 
   void play(double playerPosition) {
-    print("play - chat $chatTime player $playerPosition");
-    playing = true;
+    context.read<AppStatus>().play();
+    chatTime = playerPosition;
     timer = Timer.periodic(updatePeriod, (Timer timer) {
       if (comments != null) {
         setState(() {
+          chatTime += chatSpeed * updatePeriod.inMilliseconds / 1000;
           forwardMessageIndex(playerPosition);
         });
       }
     });
-    Provider.of<AppStatus>(context, listen: false).play();
   }
 
   void pause(double playerPosition) {
-    print("pause - chat $chatTime player $playerPosition");
-    print("index $nextMessageIndex");
-    playing = false;
+    context.read<AppStatus>().pause();
     timer?.cancel();
-    Provider.of<AppStatus>(context, listen: false).pause();
   }
 
   void adjustTimer(double playerPosition) {
-    print("timer - chat $chatTime player $playerPosition");
     if (playing) {
       pause(playerPosition);
       play(playerPosition);
@@ -82,7 +83,6 @@ class _ChatState extends State<Chat> {
   }
 
   void adjustSpeed(double playerSpeed) {
-    print("speed - chat $chatSpeed player $playerSpeed");
     setState(() {
       bool wasPlaying = playing;
       if (wasPlaying) pause(chatTime);
@@ -93,7 +93,6 @@ class _ChatState extends State<Chat> {
   }
 
   void seek(double playerPosition) {
-    print("seek - chat $chatTime player $playerPosition");
     setState(() {
       bool wasPlaying = playing;
       if (wasPlaying) pause(playerPosition);
@@ -103,15 +102,8 @@ class _ChatState extends State<Chat> {
     });
   }
 
-  double elapsedTimerDuration() {
-    return timer == null
-        ? 0
-        : chatSpeed * timer!.tick * updatePeriod.inMilliseconds / 1000;
-  }
-
   void forwardMessageIndex(double startPosition) {
     if (comments != null) {
-      chatTime = startPosition + elapsedTimerDuration();
       while (nextMessageIndex < comments!.length - 1 &&
           comments![nextMessageIndex].contentOffsetSeconds! <
               chatTime + chatOffset) {
@@ -122,40 +114,12 @@ class _ChatState extends State<Chat> {
 
   void backwardMessageIndex(double startPosition) {
     if (comments != null) {
-      chatTime = startPosition + elapsedTimerDuration();
       while (nextMessageIndex > 0 &&
           comments![nextMessageIndex - 1].contentOffsetSeconds! >
               chatTime + chatOffset) {
         nextMessageIndex--;
       }
     }
-  }
-
-  void retrieveComments() async {
-    ChatModel chat = ChatModel.fromJson(await AmqpInterface().retriveChat());
-
-    setState(() {
-      streamer = chat.streamer;
-      comments = chat.comments;
-      fetchBadges();
-      fetchEmotes();
-      fetchCheerEmotes();
-    });
-  }
-
-  void fetchBadges() async {
-    badges = TwitchBadges(streamer: streamer);
-    badges!.fetchBadges();
-  }
-
-  void fetchEmotes() async {
-    bttvEmotes = BTTVEmotes(streamer: streamer);
-    bttvEmotes!.fetchEmotes();
-  }
-
-  void fetchCheerEmotes() async {
-    cheerEmotes = TwitchCheerEmotes(streamer: streamer);
-    cheerEmotes!.fetchEmotes();
   }
 
   void setupSync() {
@@ -176,24 +140,6 @@ class _ChatState extends State<Chat> {
         nextMessageIndex);
   }
 
-  bool loaded() {
-    if (!initStatus) {
-      initStatus = comments != null &&
-          badges != null &&
-          badges!.initialized() &&
-          bttvEmotes != null &&
-          bttvEmotes!.initialized() &&
-          cheerEmotes != null &&
-          cheerEmotes!.initialized();
-      if (initStatus) {
-        WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
-          Provider.of<AppStatus>(context, listen: false).didLoad();
-        });
-      }
-    }
-    return initStatus;
-  }
-
   @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance!.addPostFrameCallback((_) {
@@ -201,22 +147,17 @@ class _ChatState extends State<Chat> {
         scrollController.jumpTo(scrollController.position.maxScrollExtent);
       }
     });
-    return loaded()
-        ? ScrollConfiguration(
-            behavior:
-                ScrollConfiguration.of(context).copyWith(scrollbars: false),
-            child: ListView.builder(
-                controller: scrollController,
-                itemCount: activeComments()?.length ?? 0,
-                itemBuilder: (BuildContext context, int index) {
-                  return ChatMessage(
-                      streamer: streamer,
-                      comment: activeComments()?[index],
-                      badges: badges);
-                }),
-          )
-        : const Center(
-            child: CircularProgressIndicator(),
-          );
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: ListView.builder(
+          controller: scrollController,
+          itemCount: activeComments()?.length ?? 0,
+          itemBuilder: (BuildContext context, int index) {
+            return ChatMessage(
+                streamer: streamer,
+                comment: activeComments()?[index],
+                badges: badges);
+          }),
+    );
   }
 }
